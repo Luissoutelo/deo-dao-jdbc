bdb25d06552aaaa89a1b9f74642c23abe913995d
Luis Soutelo <luissoutelo14@gmail.com>
2023-04-10 23:45:16 +0100

    FindById implementacion

--- ProjetoBancoDados/src/application/Program.java
+++ ProjetoBancoDados/src/application/Program.java
@@ -1,22 +1,15 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-	Department obj= new Department(1,"books");
-	
-	System.out.println(obj);
-
-	Seller seller= new Seller(21,"bob","bob@gmail.com",LocalDate.parse("2010-11-12"),3000.0,obj);
 	
 	SellerDao sellerDao=DaoFactory.createSellerDao();
+	Seller seller=sellerDao.finfById(3);
 	
 	System.out.println(seller);
 	}
