d0c7300fb1f751d7a23522561926e2c1b7e67f8c
Luis Soutelo <luissoutelo14@gmail.com>
2023-04-10 23:52:54 +0100

    Reusing intanciation

--- ProjetoBancoDados/src/model/dao/impl/SellerDaoJDBC.java
+++ ProjetoBancoDados/src/model/dao/impl/SellerDaoJDBC.java
@@ -48,16 +48,8 @@ public SellerDaoJDBC(Connection conn) {
 		st.setInt(1, id);
 		rs=st.executeQuery();
 		if(rs.next()) {
-		Department dep= new Department();
-		dep.setId(rs.getInt("DepartmentId"));
-		dep.setName(rs.getString("DepName"));
-		Seller obj=new Seller();
-		obj.setId(rs.getInt("Id"));
-		obj.setName(rs.getString("Name"));
-		obj.setEmail(rs.getString("Email"));
-		obj.setBaseSalary(rs.getDouble("BaseSalary"));
-		obj.setBirthDate(rs.getDate("BirthDate"));
-		obj.setDepartment(dep);
+		Department dep= intanciateDepartment(rs);
+		Seller obj=instanciateSeller(rs,dep);
 		return obj;
 		}
 		return null;
@@ -71,6 +63,22 @@ public SellerDaoJDBC(Connection conn) {
 
 	}
 
+	private Seller instanciateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj= new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+	}
+	private Department intanciateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
 	@Override
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
